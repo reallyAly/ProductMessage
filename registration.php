7c64ef8acebd34d1154266bf0e1bfced6a0b953e
Alysson Victor <developeralysson@gmail.com>
2021-02-20 16:03:32 -0300

    Standard structure of a Magento module

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @author Alysson Victor (github.com/reallyAly)
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, "Aly_ProductMessage", __DIR__);
